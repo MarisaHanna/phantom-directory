bfb67a8395fcf3fff16b55b116e9424ccf1489d2
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-06-23 09:39:30 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './Components/Header/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
      <h1>please work!!</h1>
      <h1>working</h1>
+     <p>not working</p>
     </div>
   );
 }
---
+++ src/Components/Header/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+
+export default function Header() {
+    return (
+       
+        <header className="jumbotron jumbotron-fluid">
+            <h4 className="header">Phantom Solutions</h4>
+            <p>Employee Directory</p>
+          
+        </header>
+    )
+}
